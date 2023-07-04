248f4a334232373462fd85fa7e44aea13f88d35c
Blake Eriks <blakeeriks.dev@gmail.com>
2023-07-03 21:07:12 -0400

    init repo

---
+++ src/hook/habitData.ts
@@ -0,0 +1,99 @@
+import { camelCase } from 'lodash'
+import { useEffect, useState } from 'react'
+
+export type HabitData = {
+  date: Date
+  [key: string]: Date | number | boolean
+  count: number
+}
+
+export type HabitHeader = {
+  key: string
+  icon: string | undefined
+  label: string
+  datatype: string
+}
+
+const parseTime = (value: string) => {
+  const [hour, minute] = value.split(':')
+  const date = new Date()
+  date.setHours(Number(hour), Number(minute), 0, 0)
+  return date
+}
+
+type ParserByType = {
+  [key: string]: (value: string) => boolean | number | Date
+}
+
+const parserByType: ParserByType = {
+  boolean: (value: string) => value === 'TRUE',
+  number: (value: string) => Number(value),
+  time: (value: string) => parseTime(value),
+}
+
+const HABIT_DATA_URL =
+  'https://docs.google.com/spreadsheets/d/e/2PACX-1vQuGKRM2RxWTaV4laTKCyUvtNBzqXlImi-D-Y9vCqMjB-z_GSDwodNZv0-ePFJOgQGAvJIeNlhah_7j/pub?output=csv'
+
+const useHabitData = () => {
+  const [rawHabitData, setRawHabitData] = useState<string | null>(localStorage.getItem('data'))
+  const [habitData, setHabitData] = useState<HabitData[]>([])
+  const [habitHeaders, setHabitHeaders] = useState<HabitHeader[]>([])
+
+  useEffect(() => {
+    if (!rawHabitData) {
+      fetch(HABIT_DATA_URL)
+        .then(res => res.text())
+        .then(data => {
+          setRawHabitData(data)
+          localStorage.setItem('data', data)
+        })
+    } else {
+      const { parsedData, headers } = parseData(rawHabitData)
+      setHabitData(parsedData)
+      setHabitHeaders(headers)
+    }
+  }, [rawHabitData])
+
+  return { habitData, habitHeaders }
+}
+
+const parseData = (rawData: string) => {
+  const splitData = rawData.split('\r\n').map((row: string) => row.split(','))
+  const parsedData: HabitData[] = []
+
+  const headers = []
+  for (let i = 2; i < splitData.length; i++) {
+    const split = splitData[i][0].split(' ')
+    const datatype = splitData[i][1]
+    const icon = split.shift()
+    const label = split.join(' ')
+    headers.push({ icon, label, key: camelCase(label), datatype })
+  }
+
+  let month
+  // Skip header declaration
+  for (let i = 2; i < splitData[0].length; i++) {
+    month = splitData[0][i].split(' ')[1] ?? month
+    const day = splitData[1][i]
+    const date = new Date(`${month} ${day}, 2023`)
+
+    // End if date is in the future
+    if (date > new Date()) break
+
+    const item: HabitData = { date, count: 0 }
+
+    for (let j = 2; j < splitData.length; j++) {
+      const { datatype, key } = headers[j - 2]
+      const parser = parserByType[datatype]
+      const value = splitData[j][i].trim()
+      const parsedValue = parser(value)
+      item[key] = parsedValue
+      if (parsedValue) item.count++
+    }
+    parsedData.push(item as HabitData)
+  }
+
+  return { parsedData, headers }
+}
+
+export default useHabitData
