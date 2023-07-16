7a35d7824864420eeefdea8cb38e0021a2ad95f2
Blake Eriks <blakeeriks.dev@gmail.com>
2023-07-15 21:30:33 -0400

    Progress

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import { filter, groupBy, reject, sumBy, values } from 'lodash'
-import { RefreshCcw, User } from 'lucide-react'
+import { Loader2, RefreshCcw, User } from 'lucide-react'
 import { useState } from 'react'
 import DailyBarChart from './DailyBarChart'
+import HabitCard from './HabitCard'
+import MonthlyBarChart from './MonthlyBarChart'
 import Nav from './Nav'
 import { Button } from './components/ui/button'
 import { Card, CardContent, CardHeader, CardTitle } from './components/ui/card'
@@ -14,31 +16,36 @@ import {
   SelectTrigger,
   SelectValue,
 } from './components/ui/select'
-import useHabitData, { HabitData, HabitHeader } from './hook/habitData'
+import useHabitData from './hook/habitData'
 import { timeDifference } from './util/time'
 
-type DurationOptions = 'oneWeek' | 'twoWeeks' | 'oneMonth' | 'threeMonths' | 'sixMonths' | 'oneYear'
 const months = [
-  { icon: '❄️', label: 'January' },
-  { icon: '🌹', label: 'Febuary' },
-  { icon: '☘️', label: 'March' },
-  { icon: '🌺', label: 'April' },
-  { icon: '🐝', label: 'May' },
-  { icon: '🌼', label: 'June' },
-  { icon: '☀️', label: 'July' },
-  { icon: '🍎', label: 'August' },
-  { icon: '🍁', label: 'September' },
-  { icon: '🎃', label: 'October' },
-  { icon: '🦃', label: 'November' },
-  { icon: '🎄', label: 'December' },
+  { key: '0', icon: '❄️', label: 'January' },
+  { key: '1', icon: '🌹', label: 'Febuary' },
+  { key: '2', icon: '☘️', label: 'March' },
+  { key: '3', icon: '🌺', label: 'April' },
+  { key: '4', icon: '🐝', label: 'May' },
+  { key: '5', icon: '🌼', label: 'June' },
+  { key: '6', icon: '☀️', label: 'July' },
+  { key: '7', icon: '🍎', label: 'August' },
+  { key: '8', icon: '🍁', label: 'September' },
+  { key: '9', icon: '🎃', label: 'October' },
+  { key: '10', icon: '🦃', label: 'November' },
+  { key: '11', icon: '🎄', label: 'December' },
 ]
 
 const now = new Date()
 
+const Overlay = ({ children }: any) => (
+  <div className='absolute z-50 h-full w-full flex justify-center items-center bg-slate-500 bg-opacity-10'>
+    {children}
+  </div>
+)
+
 const App = () => {
-  const { habitData, habitHeaders, lastFetched, refreshData } = useHabitData()
+  const { habitData, habitHeaders, lastFetched, refreshData, isFetching } = useHabitData()
   const dataByMonth = groupBy(habitData, ({ date }: { date: Date }) => date.getMonth())
-  const [context, setContext] = useState<string>(months[new Date().getMonth()].label)
+  const [context, setContext] = useState<string>(months[now.getMonth()].key)
   const volumeByMonth: { month: string; count: number; projected?: number }[] = values(
     dataByMonth
   ).map((dataset, index) => ({
@@ -46,19 +53,22 @@ const App = () => {
     count: sumBy(dataset, 'count'),
   }))
 
-  const day = new Date().getDate()
+  const day = now.getDate()
   const last = volumeByMonth[volumeByMonth.length - 1]
   if (last) {
     last.projected = Math.floor((last.count / day) * 31)
   }
 
   const numericHeaders = reject(habitHeaders, { datatype: 'boolean' })
-  const dataThisMonth = filter(habitData, ({ date }) => date.getMonth() === now.getMonth())
+  const dataThisMonth = filter(
+    habitData,
+    ({ date }) => context === 'year' || String(date.getMonth()) === context
+  )
   const compactedDataThisMonth = filter(
     dataThisMonth,
     ({ date }) => date.getDate() <= now.getDate()
   )
-  console.log(dataThisMonth)
+  console.log(habitHeaders)
 
   // const pushupsAverage = (
   //   sum(
@@ -71,10 +81,15 @@ const App = () => {
 
   return (
     <div className='flex items-center justify-center h-screen w-full'>
-      <div className='flex-1 shadow-lg bg-background border max-w-5xl space-y-4 rounded-lg'>
+      <div className='relative flex-1 shadow-lg bg-background border max-w-5xl space-y-4 rounded-lg'>
+        {isFetching && (
+          <Overlay>
+            <Loader2 className='animate-spin' />
+          </Overlay>
+        )}
         <div className='flex h-16 items-center justify-between border-b p-4'>
           <div className='flex space-x-4'>
-            <Select value={context} onValueChange={setContext}>
+            <Select value={`${context}`} onValueChange={setContext}>
               <SelectTrigger className='w-40 text-md'>
                 <SelectValue />
               </SelectTrigger>
@@ -87,8 +102,8 @@ const App = () => {
                   {months
                     .slice(0, now.getMonth() + 1)
                     .reverse()
-                    .map(({ icon, label }, index) => (
-                      <SelectItem key={index} value={label} className='text-md'>
+                    .map(({ icon, label, key }, index) => (
+                      <SelectItem key={index} value={key} className='text-md'>
                         {icon} {label}
                       </SelectItem>
                     ))}
@@ -114,7 +129,11 @@ const App = () => {
               <CardHeader>Habit Volume</CardHeader>
             </CardTitle>
             <CardContent className='pb-2'>
-              <DailyBarChart data={dataThisMonth} keys={['count']} />
+              {context === 'year' ? (
+                <MonthlyBarChart data={volumeByMonth} />
+              ) : (
+                <DailyBarChart data={dataThisMonth} keys={['score']} />
+              )}
             </CardContent>
           </Card>
           <div className='flex w-1/2 space-x-4'>
@@ -132,52 +151,6 @@ const App = () => {
         </div>
       </div>
     </div>
-  )
-}
-
-type HabitCardData = {
-  average?: number | string
-  sum?: number
-}
-
-const HabitCard = ({ habit, data }: { habit: HabitHeader; data: HabitData[] }) => {
-  if (!habit || !data) return null
-
-  const { label, icon, key, datatype } = habit
-  const cardData: HabitCardData = {}
-
-  switch (datatype) {
-    case 'boolean':
-      cardData.average = sumBy(data, key)
-      break
-    case 'number':
-      cardData.average = sumBy(data, key)
-      break
-    case 'time': {
-      const avgInMs =
-        data.reduce((acc, data) => acc + (data[key] as Date).getTime(), 0) / data.length
-      const avgAsDate = new Date(avgInMs)
-      const averageHours = avgAsDate.getHours()
-      const averageMinutes = avgAsDate.getMinutes()
-      cardData.average = `${averageHours}:${averageMinutes < 10 ? '0' : ''}${averageMinutes}`
-      break
-    }
-  }
-
-  return (
-    <Card className='flex-1'>
-      <CardTitle className='relative border-b'>
-        <CardHeader className='font-light text-center h-16 justify-center px-2 py-0'>
-          {label}
-        </CardHeader>
-        <div className='absolute top-0 left-0 text-[16px] p-1 border-b border-r'>{icon}</div>
-      </CardTitle>
-      <CardContent className='pb-0'>
-        <h1 className='text-2xl text-slate-800 text-center leading-relaxed font-semibold'>
-          {cardData.average}
-        </h1>
-      </CardContent>
-    </Card>
   )
 }
 
