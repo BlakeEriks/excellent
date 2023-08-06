ea244d78c56a6ab99e0d6a8c77d4efd76bdf97d9
Blake Eriks <blakeeriks.dev@gmail.com>
2023-08-06 14:15:25 -0400

    Add goals to habit card

--- src/HabitCard.tsx
+++ src/HabitCard.tsx
@@ -1,36 +1,55 @@
-import { sumBy } from 'lodash'
-import { Card, CardContent, CardHeader, CardTitle } from './components/ui/card'
-import { HabitData, HabitHeader } from './hook/habitData'
+import { chain, sum } from 'lodash'
+import { StarIcon } from 'lucide-react'
+import Status from './components/Status'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from './components/ui/card'
+import { HabitHeader } from './hook/habitData'
+import { formatTime } from './util/time'
 
+type HabitCardProps = HabitHeader & {
+  data: (number | boolean | Date)[]
+  goal: number | boolean | Date
+}
 type HabitCardData = {
   average?: number | string
   sum?: number
+  goalLabel?: string
+  goalStatus?: boolean
 }
 
-const HabitCard = ({ habit, data }: { habit: HabitHeader; data: HabitData[] }) => {
-  if (!habit || !data) return null
+const now = new Date()
 
-  const { label, icon, key, datatype } = habit
+const HabitCard = ({ label, icon, datatype, data, goal }: HabitCardProps) => {
+  if (!data?.length) return null
+
   const cardData: HabitCardData = {}
 
   switch (datatype) {
     case 'boolean':
     case 'number':
-      cardData.average = parseFloat(sumBy(data, key).toFixed(1))
+      cardData.average = parseFloat(sum(data).toFixed(1))
+      console.log(((goal as number) * 31) / now.getDate())
+      cardData.goalStatus = ((goal as number) * now.getDate()) / 31 <= cardData.average
+      cardData.goalLabel = `${goal}`
       break
     case 'time': {
-      const avgInMs =
-        data.reduce((acc, data) => {
-          const date = data[key] as Date
-          return acc + (date ? date.getTime() : 0)
-        }, 0) / data.length
-      const avgAsDate = new Date(avgInMs)
-      const averageHours = avgAsDate.getHours()
-      const averageMinutes = avgAsDate.getMinutes()
-      cardData.average = `${averageHours}:${averageMinutes < 10 ? '0' : ''}${averageMinutes}`
+      const avgInMs = chain(data)
+        .compact()
+        .meanBy(date => (date as Date).getTime())
+        .value()
+      cardData.average = formatTime(new Date(avgInMs))
+      cardData.goalStatus = avgInMs <= (goal as Date).getTime()
+      cardData.goalLabel = formatTime(goal as Date)
       break
     }
   }
+  console.log(goal)
 
   return (
     <Card className='flex-1'>
@@ -42,9 +61,20 @@ const HabitCard = ({ habit, data }: { habit: HabitHeader; data: HabitData[] }) =
       </CardTitle>
       <CardContent className='pb-0'>
         <h1 className='text-2xl text-slate-800 text-center leading-relaxed font-semibold'>
+          <Status status={cardData.goalStatus ? 'green' : 'red'} />
           {cardData.average}
         </h1>
       </CardContent>
+      {goal && (
+        <CardFooter className='justify-end'>
+          <CardDescription>
+            <div className='flex items-center'>
+              <StarIcon fill='#64748b' size={12} />
+              <span>{cardData.goalLabel}</span>
+            </div>
+          </CardDescription>
+        </CardFooter>
+      )}
     </Card>
   )
 }
